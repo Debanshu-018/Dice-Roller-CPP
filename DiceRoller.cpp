ae3bdfacdad2521a02f3a333a0ea4f5f97044bef
Debanshu Jena <jenadebanshu419@gmail.com>
2026-07-22 21:28:44 +0530

    Created a Dice Roller game in C++ using random number generation. Added ASCII art for each dice face and an option to roll repeatedly until the user exits.

---
+++ DiceRoller.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    char choice;
+
+    // Seed the random number generator
+    srand(time(0));
+
+    cout << "===================================" << endl;
+    cout << "       DICE ROLLER SIMULATOR       " << endl;
+    cout << "===================================" << endl;
+
+    do {
+        int dice = rand() % 6 + 1;
+
+        cout << "\n🎲 You rolled: " << dice << endl;
+
+        switch (dice) {
+            case 1:
+                cout << "-----\n";
+                cout << "|   |\n";
+                cout << "| * |\n";
+                cout << "|   |\n";
+                cout << "-----\n";
+                break;
+
+            case 2:
+                cout << "-----\n";
+                cout << "|*  |\n";
+                cout << "|   |\n";
+                cout << "|  *|\n";
+                cout << "-----\n";
+                break;
+
+            case 3:
+                cout << "-----\n";
+                cout << "|*  |\n";
+                cout << "| * |\n";
+                cout << "|  *|\n";
+                cout << "-----\n";
+                break;
+
+            case 4:
+                cout << "-----\n";
+                cout << "|* *|\n";
+                cout << "|   |\n";
+                cout << "|* *|\n";
+                cout << "-----\n";
+                break;
+
+            case 5:
+                cout << "-----\n";
+                cout << "|* *|\n";
+                cout << "| * |\n";
+                cout << "|* *|\n";
+                cout << "-----\n";
+                break;
+
+            case 6:
+                cout << "-----\n";
+                cout << "|* *|\n";
+                cout << "|* *|\n";
+                cout << "|* *|\n";
+                cout << "-----\n";
+                break;
+        }
+
+        cout << "\nRoll again? (Y/N): ";
+        cin >> choice;
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "\nThank you for playing! 🎲" << endl;
+
+    return 0;
+}

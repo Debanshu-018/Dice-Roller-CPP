827d699e00b18d69bcb6533ec94733d5e09d6261
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-13 18:27:19 +0530

    Implement missing number finder in Day1.cpp
    Add a program to find the missing number in an array.

--- Day1.cpp
+++ Day1.cpp
@@ -100,7 +100,24 @@ int main() {
 
     return 0;
 }
+// Q5. Find the Missing Number
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {1, 2, 3, 5};
+    int n = 5;
+
+    int total = n * (n + 1) / 2;
+    int sum = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        sum += arr[i];
+
+    cout << "Missing number = " << total - sum;
+
+    return 0;
+}
 
 
 

c89efd6555aa57c87b16a2d26a4ef6b56d358508
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-13 18:23:23 +0530

    Add array reversal functionality in Day1.cpp
    Implemented a function to reverse an array and print it.

--- Day1.cpp
+++ Day1.cpp
@@ -24,7 +24,30 @@ int main() {
 }
 
 
-// Q2. Reverse an Array2. Reverse an Array
+// Q2.Reverse an Array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    cout << "Reversed array: ";
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
 
 
 

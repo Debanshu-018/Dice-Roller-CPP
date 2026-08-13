a6a8b6688724d86f12d47bb463918abaa3ea5f34
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-13 18:24:11 +0530

    Implement second largest element finder in Day1.cpp
    Added a program to find the second largest element in an array.

--- Day1.cpp
+++ Day1.cpp
@@ -49,6 +49,33 @@ int main() {
     return 0;
 }
 
+// 3. Find the Second Largest Element
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 5, 20, 8, 15};
+    int n = 5;
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Second largest = " << secondLargest;
+
+    return 0;
+}
+
 
 
 

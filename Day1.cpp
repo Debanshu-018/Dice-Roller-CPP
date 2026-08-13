c0875f25d42f5ebb036fd32091e19f1a1c76c09b
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-13 18:21:52 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,1 +1,24 @@
+// Q1. Find Maximum and Minimum Element
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int n = 5;
+
+    int maximum = arr[0];
+    int minimum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maximum)
+            maximum = arr[i];
+
+        if (arr[i] < minimum)
+            minimum = arr[i];
+    }
+
+    cout << "Maximum = " << maximum << endl;
+    cout << "Minimum = " << minimum << endl;
+
+    return 0;
+}

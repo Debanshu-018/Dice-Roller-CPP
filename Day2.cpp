23d5a133efdae37fcc762731334657a7ee564270
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-15 23:01:18 +0530

    Implement Tower of Hanoi algorithm

--- Day2.cpp
+++ Day2.cpp
@@ -89,6 +89,38 @@ int main() {
 
     return 0;
 }
+// Q4. Tower of Hanoi.
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+
+    if (n == 1) {
+        cout << "Move disk 1 from "
+             << source << " to "
+             << destination << endl;
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+
+    cout << "Move disk " << n
+         << " from " << source
+         << " to " << destination << endl;
+
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
 
 
 

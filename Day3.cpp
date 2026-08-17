6d572e2df04b8ce2428a02414edb1d1f6d710e44
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-17 20:49:35 +0530

    Implement linear search in Day3.cpp

---
+++ Day3.cpp
@@ -0,0 +1,18 @@
+// Q1.Linear Search
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5, key = 30;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at index " << i;
+            return 0;
+        }
+    }
+
+    cout << "Element not found";
+    return 0;
+}

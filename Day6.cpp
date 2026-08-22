b62adec8434fb918989c14340cf8b71f2894b7ef
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-22 19:36:48 +0530

    Implement sum calculation for array elements
    Added a program to find the sum of array elements.

--- Day6.cpp
+++ Day6.cpp
@@ -66,7 +66,22 @@ int main() {
 }
 
 // Q4. Find Sum of Array Elements
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
 
 
 

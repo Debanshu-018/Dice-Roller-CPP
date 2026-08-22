7f675bbe309b661ab0418a39dc369325b7bcf0f7
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-22 19:34:43 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -43,8 +43,27 @@ int main() {
     return 0;
 }
 
-Q3. Reverse an Array
+// Q3. Reverse an Array
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int left = 0, right = n - 1;
+
+    while (left < right) {
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
 
 
 

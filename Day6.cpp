3a15e881cf4e932bcb973138ef38db30b716b85a
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-22 19:38:50 +0530

    Add function to find second largest element

--- Day6.cpp
+++ Day6.cpp
@@ -105,8 +105,32 @@ int main() {
 
     return 0;
 }
+// Q6. Find Second Largest Element
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main() {
+    int arr[] = {10, 5, 20, 8, 15};
+    int n = 5;
 
+    int largest = INT_MIN;
+    int second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    cout << "Second Largest = " << second;
+
+    return 0;
+}
 
 
 
